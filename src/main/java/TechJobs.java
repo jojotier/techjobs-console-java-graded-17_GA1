79442b3b9e8839b5b63d58428c88a1aa43404020
jojotier <122423763+jojotier@users.noreply.github.com>
2023-08-21 20:07:09 -0500

    implemented the printJobs() function!

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -120,6 +120,8 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        for(int i = 0; i < someJobs.size(); i++){
+        System.out.println("*****\n position type: " + someJobs.get(i).get("position type") + "\n name: " + someJobs.get(i).get("name") + "\n employer: " + someJobs.get(i).get("employer") + "\n location: " + someJobs.get(i).get("location") + "\n core competency: " + someJobs.get(i).get("core competency") + "\n*****");
+        }
     }
 }
